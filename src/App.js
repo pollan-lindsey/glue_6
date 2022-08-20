cfa17c4a5e30d26c076dde247747743f6d8aa75a
LINDSEY POLLAN <LPOLLAN@LIVE.COM>
2022-08-20 14:53:24 -0500

    key stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+constructor(props) {
+  super(props)
+
+  this.state = {
+    asteroid: '',
+    // first_observation_date: '',
+  }
+  //above is named by nasa
 }
 
-export default App;
+handleSubmit = async () => {
+  
+  const API = 'https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key={process.env.REACT_APP_ASTEROID}'
+  const res= await axios.get(API);
+  console.log(res.data)
+  this.setState({asteroid: res.data.asteroid})
+  // this.setState( {first_observation_date: res.data.first_observation_date}) //need multiple things here OR reference an array in state
+}
+
+// handleObservation = () => {
+//   console.log(res.data)
+//   this.setState({first_observation_date: res.data.first_observation_date})
+// }
+  
+  render() {
+    return (
+      <div>
+        <h1>Asteroid locater</h1>
+        <button onClick={this.handleSubmit}>Locate Asteroid</button>
+        <h2>{this.state.asteroid}</h2>
+          <ul>
+            <li>Name: {this.state.asteroid}</li>
+            <li>First Observation Date: {this.state.first_observation_date}</li>
+          </ul>
+      </div>
+    )
+  }
+}
+
